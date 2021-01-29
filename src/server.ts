6f1bd2a4dbd7749606be86632675f44474f1e576
Don Tay Xi Cheng <don.tay@shopback.com>
2021-01-29 21:16:55 +0800

    Add template engine

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,18 @@ dotenv.config({ path: 'config/.env' });
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const port = parseInt(process.env.PORT) || 5000;
+
+app.get('/', (req, res) => {
+  const payload = {
+    pageTitle: 'Tweeter',
+  };
+
+  res.status(200).render('home', payload);
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`.yellow.bold);
