4b5a4dd90d56717faec8a197f730317048ef6aac
Don Tay Xi Cheng <don.tay@shopback.com>
2021-04-05 13:10:59 +0800

    Add login page

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,3 +4,7 @@ export const authRouter = express.Router();
 authRouter.get('/login', (req, res, next) => {
     res.status(200).render('login');
 });
+
+authRouter.get('/register', (req, res, next) => {
+    res.status(200).render('register');
+});
