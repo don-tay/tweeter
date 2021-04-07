7d976b3abd53941399210d40b58517fb367c31da
Don Tay Xi Cheng <don.tay@shopback.com>
2021-04-07 21:26:16 +0800

    Add auth validation, pending frontend error display

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,18 +1,56 @@
+import { IsEmail, IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 import express from 'express';
+import { PASSWORD_REGEX } from '../constants';
+import { asyncHandler } from '../middlewares/asyncHandler.middleware';
+import { validateModel } from '../utilities';
 export const authRouter = express.Router();
+
+class RegisterUserDto {
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+    @IsString()
+    @Length(5, 20)
+    username: string;
+    @IsEmail()
+    email: string;
+    @Length(8, 25)
+    @Matches(PASSWORD_REGEX)
+    password: string;
+}
+
+class LoginUserDto {
+    @IsString()
+    @IsNotEmpty()
+    loginUsername: string;
+    @IsString()
+    @IsNotEmpty()
+    loginPassword: string;
+}
 
 authRouter.get('/login', (req, res, next) => {
     res.status(200).render('login');
 });
 
 authRouter.post('/login', (req, res, next) => {
-    res.status(200).render('login');
+    const { loginUsername, loginPassword } = req.body;
+    const payload = { loginUsername: loginUsername.trim(), loginPassword };
+    res.status(200).render('login', payload);
 });
 
 authRouter.get('/register', (req, res, next) => {
     res.status(200).render('register');
 });
 
-authRouter.post('/register', (req, res, next) => {
-    res.status(200).render('register');
-});
+authRouter.post(
+    '/register',
+    asyncHandler(async (req, res, next) => {
+        const { firstName, lastName, username, email, password } = req.body;
+        const payload = { firstName: firstName.trim(), lastName: lastName.trim(), username: username.trim(), email: email.trim(), password };
+        await validateModel(RegisterUserDto, payload);
+        res.status(200).render('register', payload);
+    }),
+);
