e6c7bc2d435beb0d0f15c9f04c4882d9c928d873
Don Tay Xi Cheng <don.tay@shopback.com>
2021-04-19 00:51:46 +0800

    Add show post on receive response from post api call

--- public/js/common.js
+++ public/js/common.js
@@ -20,6 +20,33 @@ $('#submitPostButton').click((event) => {
     };
 
     $.post('/api/posts', data, (response, status, xhr) => {
-        console.log(response);
+        const html = createPostHtml(response.data);
+        $('.postsContainer').prepend(html);
+        textbox.val('');
+        button.prop('disabled', true);
     });
 });
+
+function createPostHtml(postData) {
+    const { postedBy, createdAt } = postData;
+    const displayName = postedBy.firstName + postedBy.lastName;
+    return `<div class='post'>
+                <div class='mainContentContainer'>
+                    <div class='userImageContainer'>
+                        <img src='${postedBy.profilePic}' />
+                    </div>
+                    <div class='postContentContainer'>
+                        <div class='header'>
+                            <a href='/profile/${postedBy.username}'>${displayName}</a>
+                            <span class='username'>@${postedBy.username}</span>
+                            <span class='date'>${createdAt}</span>
+                        </div>
+                        <div class='postBody'>
+                            <span>${postData.content}</span>
+                        </div>
+                        <div class='postFooter'>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
