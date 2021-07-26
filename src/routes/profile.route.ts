65aa22c1a1e53a28ca25e3b7745d2fcc3c6daec6
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-07-26 17:06:27 +0800

    Change get profile endpoint to accept mongoid and username

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -1,12 +1,19 @@
+import { isMongoId } from 'class-validator';
 import express from 'express';
 import { isEmpty } from 'lodash';
+import { LeanDocument } from 'mongoose';
 import { requireLogin } from '../middlewares';
-import { User } from '../schemas/user.schema';
+import { IUser, User } from '../schemas/user.schema';
 export const profilesRouter = express.Router();
 
-profilesRouter.get('/:username', requireLogin, async (req, res) => {
-    const { username } = req.params;
-    const user = await User.findOne({ username }).lean().exec();
+profilesRouter.get('/:usernameOrId', requireLogin, async (req, res) => {
+    const { usernameOrId } = req.params;
+    let user: LeanDocument<IUser>;
+    if (isMongoId(usernameOrId)) {
+        user = await User.findById(usernameOrId).lean().exec();
+    } else {
+        user = await User.findOne({ username: usernameOrId }).lean().exec();
+    }
 
     const pageTitle = !isEmpty(user) ? user.username : 'User not found';
 
