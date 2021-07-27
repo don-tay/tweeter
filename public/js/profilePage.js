1592be508b0e68633b34780e5d26e077cc6b835f
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-07-27 18:01:39 +0800

    Add profile page with post and reply tabs

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -1,9 +1,20 @@
 $(document).ready(() => {
-    getPosts();
+    console.log(selectedTab);
+    if (selectedTab === 'posts') {
+        getPosts();
+    } else if (selectedTab === 'replies') {
+        getReplies();
+    }
 });
 
 function getPosts() {
-    $.get(`/api/posts/`, { postedBy: profileUserId }, (response) => {
+    $.get(`/api/posts`, { postedBy: profileUserId, excludeReplies: true }, (response) => {
+        outputPosts(response.data, $('.postsContainer'));
+    });
+}
+
+function getReplies() {
+    $.get(`/api/posts/${profileUserId}/replies`, (response) => {
         outputPosts(response.data, $('.postsContainer'));
     });
 }
--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -6,6 +6,28 @@ import { requireLogin } from '../middlewares';
 import { IUser, User } from '../schemas/user.schema';
 export const profilesRouter = express.Router();
 
+// if replies tab selected, add payload selectedTab: 'replies'
+profilesRouter.get('/:usernameOrId/replies', requireLogin, async (req, res) => {
+    const { usernameOrId } = req.params;
+    let user: LeanDocument<IUser>;
+    if (isMongoId(usernameOrId)) {
+        user = await User.findById(usernameOrId).lean().exec();
+    } else {
+        user = await User.findOne({ username: usernameOrId }).lean().exec();
+    }
+
+    const pageTitle = !isEmpty(user) ? user.username : 'User not found';
+
+    const payload = {
+        pageTitle,
+        userLoggedIn: req.session?.user,
+        profileUser: user,
+        selectedTab: 'replies',
+    };
+
+    res.status(200).render('profilePage', payload);
+});
+
 profilesRouter.get('/:usernameOrId', requireLogin, async (req, res) => {
     const { usernameOrId } = req.params;
     let user: LeanDocument<IUser>;
@@ -21,6 +43,7 @@ profilesRouter.get('/:usernameOrId', requireLogin, async (req, res) => {
         pageTitle,
         userLoggedIn: req.session?.user,
         profileUser: user,
+        selectedTab: 'posts',
     };
 
     res.status(200).render('profilePage', payload);
